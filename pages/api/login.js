eb2a0730aaf2ed6c83f76cc0e2e2ff8cc2b0c1db
hristohpavlov <hristohpavlov@gmail.com>
2023-03-06 01:48:45 +0200

    gfds

--- pages/api/login.js
+++ pages/api/login.js
@@ -7,8 +7,8 @@ export default async function handler(req, res) {
     const { email, password } = req.body;
     const user = await User.findOne({ email, password });
     if (!user) {
-        return res.redirect(307, '/unauthorized')
+        return res.redirect(302, '/unauthorized')
     } else {
-        res.redirect(307, '/admin/panel');
+        res.redirect(302, '/admin/panel');
     }
 }
